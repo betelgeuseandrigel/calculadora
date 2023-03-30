0040b2af0f3d5f5a92f23865cc9320e796365fed
luciano <luciano-14k1@hotmail.com>
2023-03-30 03:27:46 -0300

    calculadora en proceso

---
+++ Display.js
@@ -0,0 +1,55 @@
+class Display{
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.tipoDeOperacion = undefined;
+        this.valorActual = "";
+        this.valorAnterior = "";
+        this.signos = {
+            sumar: "+",
+            multiplicar: "x",
+            dividir: "%",
+            restar: "-",
+        }
+    }
+
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo(){
+        this.valorActual = "";
+        this.valorAnterior = "";
+        this.tipoDeOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    computar(tipo){
+        this.tipoDeOperacion !== "igual" && this.calcular();
+        this.tipoDeOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = "";
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero){
+        if(numero === "." && this.valorActual.includes(".")) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoDeOperacion] || ""}`;
+    }
+
+    calcular(){
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual) || isNaN(valorAnterior)) return
+        this.valorActual = this.calculadora[this.tipoDeOperacion](valorAnterior, valorActual);
+    }
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll(".numero");
+const botonesOperadores = document.querySelectorAll(".operador");
+const botonBorrar = document.querySelector(".delete");
+const botonBorrarTodo = document.querySelector(".deleteAll");
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
+});
+
+botonBorrar.addEventListener("click", () => display.borrar());
+
+botonBorrarTodo.addEventListener("click", () => display.borrarTodo());
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener("click", () => display.computar(boton.id));
+})
+
+
+//cambio de color de toda la pagina
+
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+})
+
